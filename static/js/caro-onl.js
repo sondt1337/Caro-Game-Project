2046419fa252a6362974523351563d6074854fde
Son Dinh Thai <sondinh99999@gmail.com>
2024-01-10 05:42:30 +0700

    update socket link --> Render

--- static/js/caro-onl.js
+++ static/js/caro-onl.js
@@ -1,5 +1,5 @@
 // Kết nối socket qua server (KHI KHỞI TẠO TRÊN SERVER)
-let socket = io.connect('https://1df23f47-644a-4a2f-92f0-e2c1ed4d8666-00-1gbez3q37p9dp.pike.replit.dev/');
+let socket = io.connect('https://caro-game-thai-son.onrender.com/');
 
 // Kết nối socket thông qua LAN (BUILD TRÊN LOCAL)
 // let socket = io.connect('http://' + document.domain + ':' + location.port);
